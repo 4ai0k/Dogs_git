231fcd31a5c5db9e00934c87cace9ec5a079f10c
peter <petermy666@gmail.com>
2024-09-14 10:56:27 +0400

    Добавлены два экземпляра класса: dog_1, dog_2

---
+++ main.py
@@ -0,0 +1,5 @@
+from dogs import Thoroughbred
+
+if __name__ == '__main__':
+    dog_1 = Thoroughbred('Шарик', '10 лет', 'коричневый', 'дворовая')
+    dog_2 = Thoroughbred('Арчи', '5 лет', 'черный', 'немецкая овчарка')
