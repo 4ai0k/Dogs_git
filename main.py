be45320a4545f91843bfc76daca0794fc3f4df7b
peter <petermy666@gmail.com>
2024-09-14 11:04:44 +0400

    Добавленый вызовы функций

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from dogs import Thoroughbred
 if __name__ == '__main__':
     dog_1 = Thoroughbred('Шарик', '10 лет', 'коричневый', 'дворовая')
     dog_2 = Thoroughbred('Арчи', '5 лет', 'черный', 'немецкая овчарка')
+
+    print(dog_1.dark())
+    print(dog_1.go_to_dog_show(), '\n')
+    print(dog_2.dark())
+    print(dog_2.go_to_dog_show())
